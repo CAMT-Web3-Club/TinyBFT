13689bb1910420a923824eac4347709fe9c86c8a
Harald Böhm <harald.boehm@fau.de>
2022-10-07 16:42:08 +0200

    bits.h: make defines architecture independent

--- src/bits.h
+++ src/bits.h
@@ -11,67 +11,29 @@ Note that unsigned arithmetic can lead to unexpected results.
 #ifndef _BITS_H
 #define _BITS_H
 
+#include <stdint.h>
+
 #include "assert.h"
 
 #define byte_bits 8
 typedef unsigned int Uint;
 
-typedef unsigned char Ubits8;
-typedef unsigned short Ubits16;
-typedef unsigned int Ubits32;
+typedef uint8_t Ubits8;
+typedef uint16_t Ubits16;
+typedef uint32_t Ubits32;
+typedef uint64_t Ubits64;
 
-typedef char Bits8;
-typedef short Bits16;
-typedef int Bits32;
+typedef int8_t Bits8;
+typedef int16_t Bits16;
+typedef int32_t Bits32;
+typedef int64_t Bits64;
 
-#ifdef __alpha
+#define INT_BITS (sizeof(int) * byte_bits)
+#define LONG_BITS (sizeof(long) * byte_bits)
 
-#define LONG_SIGN_BIT_MASK 0x8000000000000000UL
-#define INT_SIGN_BIT_MASK 0x80000000UL
+#define INT_SIGN_BIT_MASK (0x1 << (IN_BITS - 1))
+#define LONG_SIGN_BIT_MASK (0x1 << (LONG_BITS - 1))
 
-#define INT_BITS 32
-#define LONG_BITS 64
-
-#define POINTER_INT long
-
-#define _BITS_H_OK
-
-typedef long int Bits64;
-/* Lformat is the format string for printing purposes */
-#endif
-
-#ifdef __x86_64__
-
-#define LONG_SIGN_BIT_MASK 0x8000000000000000UL
-#define INT_SIGN_BIT_MASK 0x80000000UL
-
-#define INT_BITS 32
-#define LONG_BITS 64
-
-#define POINTER_INT long
-
-#define _BITS_H_OK
-
-typedef long Bits64;
-
-#endif  // __X86_64__
-
-#ifdef __i386__
-
-#define LONG_SIGN_BIT_MASK 0x80000000UL
-#define INT_SIGN_BIT_MASK 0x80000000UL
-
-#define INT_BITS 32
-#define LONG_BITS 32
-
-#define POINTER_INT int
-
-#define _BITS_H_OK
-
-typedef long long Bits64;
-
-#endif
-
-#undef _BITS_H_OK
+#define POINTER_INT intptr_t
 
 #endif  // _BITS_H
