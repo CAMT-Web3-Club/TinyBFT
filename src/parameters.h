5eb438a7e6780ac0d06f8fd7f496802290814fd1
Harald Böhm <harald.boehm@fau.de>
2022-10-25 17:35:36 +0200

    Make window size configurable
    Closes #12

--- src/parameters.h
+++ src/parameters.h
@@ -1,6 +1,10 @@
 #ifndef _parameters_h
 #define _parameters_h 1
 
+#ifndef WINDOW_SIZE
+#define WINDOW_SIZE 256
+#endif  // WINDOW_SIZE
+
 namespace libbyzea {
 
 const int Max_num_replicas = 32;
@@ -8,14 +12,16 @@ const int Max_num_replicas = 32;
 // Interval in sequence space between "checkpoint" states, i.e.,
 // states that are checkpointed and for which Checkpoint messages are
 // sent.
-const int checkpoint_interval = 128;
+constexpr int checkpoint_interval = (WINDOW_SIZE / 2);
+static_assert(checkpoint_interval > 0,
+               "Invalid window size causes checkpoint interval to be 0");
 
 // Maximum number of messages for which protocol can be
 // simultaneously in progress, i.e., messages with sequence number
 // higher than last_stable+max_out are ignored. It is required that
 // max_out > checkpoint_interval. Otherwise, the algorithm will be
 // unable to make progress.
-const int max_out = 256;
+constexpr int max_out = WINDOW_SIZE;
 
 }  // namespace libbyzea
 
