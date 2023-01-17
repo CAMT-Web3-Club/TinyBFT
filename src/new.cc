ccb144be21048ceb1316e9828e4205eab56f3b34
Harald Böhm <harald.boehm@fau.de>
2023-01-17 16:29:28 +0100

    new: add missing new and delete operators

--- src/new.cc
+++ src/new.cc
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void *operator new(size_t sz) {
-  fprintf(stderr, "new(%ld)\n", sz);
-  return malloc(sz);
-}
+void *operator new(size_t sz) { return malloc(sz); }
 
-void operator delete(void *p) { return free(p); }
+void *operator new[](size_t sz) { return operator new(sz); }
+
+void operator delete(void *p, [[maybe_unused]] size_t sz) { return free(p); }
+
+void operator delete[](void *p, size_t sz) { operator delete(p, sz); }
+
+void operator delete(void *p) { free(p); }
+
+void operator delete[](void *p) { operator delete(p); }
